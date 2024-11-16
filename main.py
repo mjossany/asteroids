7f8ee9354e4d30a5553d54b4449962b7c4a02d14
Jossany <jossany.silva@zallpy.com>
2024-11-16 17:15:44 -0300

    set fps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,7 +15,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
 
 if __name__ == "__main__":
     main()
